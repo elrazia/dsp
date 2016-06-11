690f52c26ae45d5dfc4a1ea8f8e5e0bec4418f1b
Ahmed <elrazia@gmail.com>
2016-06-11 12:41:54 -0400

    python code added

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,44 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+prof_df = pd.read_csv('faculty.csv')
+
+# Question 1
+
+uniques = {}
+deg = []
+for i in prof_df[' degree']:
+    deg.append(i.split())
+
+for i in deg:
+    for j in i:
+        j = j.replace('.','')
+        if j in uniques:
+            uniques[j] += 1
+        else:
+            uniques[j] = 1
+print uniques
+
+# Question 2
+
+titles = {}
+for i in prof_df[' title']:
+    i = i.replace(' is ',' of ')
+    if i in titles:
+        titles[i] += 1
+    else:
+        titles[i] = 1
+print titles
+
+# Question 3
+
+emails = []
+for i in prof_df[' email']:
+    emails.append(i)
+print emails
+
+# Question 4
+
+domains = []
+for email in emails:
+    index = email.find('@')
+    domains.append(email[index+1:])
+print list(set(domains))
